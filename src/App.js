6248a8bd59fa090bc8c75b108ad4d36da86106a8
eKerney <Eric.Kerney@airspacelink.com>
2022-03-21 17:27:03 -0400

    working with backend

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import * as React from 'react';
+import ReactDOM from "react-dom";
 import Map, {Source, Layer, Popup} from 'react-map-gl';
+import { MapContext } from 'react-map-gl/dist/esm/components/map';
 import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
 import {render} from 'react-dom';
 import { H3HexagonLayer } from "@deck.gl/geo-layers";
 import {ScatterplotLayer} from '@deck.gl/layers';
 import {CSVLoader} from '@loaders.gl/csv';
 import {useState, useEffect, useMemo, useCallback, useRef} from 'react';
-//import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import ControlPanel from './control-panel';
 
-const geojson = {
-  type: 'FeatureCollection',
-  features: [
-    {type: 'Feature', geometry: {type: 'Point', coordinates: [-83.05, 42.33]}}
-  ]
-};
-const layerStyle = {
-  id: 'point',
-  type: 'circle',
-  paint: {
-    'circle-radius': 20,
-    'circle-color': 'red'
-  }
-};
-const data = [
-  {sourcePosition: [-150, 41], targetPosition: [-50, 43]}
-];
-const HEX_DATA =
-  "https://raw.githubusercontent.com/chriszrc/foss4g-2021-react-mapbox/main/deck-layers-map/public/data/hex_radio_coverage.json";
-  const stationData = 'https://raw.githubusercontent.com/eKerney/reactMapTest/main/src/noaaAptEditedCol.csv';
-  const testData = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
-
-
+const HEX_DATA = "https://raw.githubusercontent.com/chriszrc/foss4g-2021-react-mapbox/main/deck-layers-map/public/data/hex_radio_coverage.json";
+const stationData = 'https://raw.githubusercontent.com/eKerney/reactMapTest/main/src/noaaAptEditedCol.csv';
+const testData = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
   
   export default function App() {
+    
     const layerStations = new ScatterplotLayer({
       id: 'scatterplot-layer',
       data: fetch(stationData)
@@ -44,40 +27,18 @@ const HEX_DATA =
       stroked: true,
       filled: true,
       radiusScale: 6,   
-      radiusMinPixels: 10,
+      radiusMinPixels: 5,
       radiusMaxPixels: 100,
       lineWidthMinPixels: 1,
       getPosition: d => [d['lon'], d['lat']],
       getRadius: d => Math.sqrt(d.exits),
       getFillColor: d => [(255-(d['elevation']*.1)), (140+(d['elevation']*.001)), (d['elevation']* .50)],
       getLineColor: d => [0, 0, 0],
-      onHover: (info, event) => console.log('Hovered:', info.object),
-      onClick: ({object}) => object
+      //onHover: (info, event) => console.log('Hovered:', info.object),
+      //onClick: (info, event) => onClick(info)
     });  
    
-    const layerUK = new ScatterplotLayer({
-      id: 'scatterplot-layer',
-      data: fetch(testData)
-        .then(response => response.arrayBuffer())
-        .then(buffer => CSVLoader.parse(buffer)),
-      pickable: true,
-      opacity: 0.8,
-      stroked: true,
-      filled: true,
-      radiusScale: 6,
-      radiusMinPixels: 10,
-      radiusMaxPixels: 100,
-      lineWidthMinPixels: 1,
-      getPosition: d => [d['lng'], d['lat']],
-      getRadius: d => Math.sqrt(d.exits),
-      getFillColor: d => [255, 140, 0],
-      getLineColor: d => [0, 0, 0]
-    });
-
-    const lineTest = new LineLayer({id: 'line-layer', data});
-
     const layers = [
-    new LineLayer({id: 'line-layer', data}),
      new H3HexagonLayer({
       id: "H3 Radio Sources",
       data: HEX_DATA,
@@ -93,108 +54,156 @@ const HEX_DATA =
     })
     ];
 
-
     const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZXJpY2tlcm5leSIsImEiOiJja2FjbTNiMXcwMXp1MzVueDFjdDNtcW92In0.LW0qdB-2FmA3UK51M67fAQ';
-    // const [viewState, setViewState] = React.useState({
-    //   longitude: -83.05,
-    //   latitude: 42.33,
-    //   zoom: 11
-    // });
-
-    const [hoverInfo, setHoverInfo] = useState();
     
     const [viewState, setViewState] = React.useState({
-      longitude: -85,
+      longitude: -95,
       latitude: 35,
-      zoom: 4,
-      pitch: 60,
-      bearing: -50,
+      zoom: 3,
+      pitch:40,
+      bearing: 0,
     });
-//***
+    
+  const [hoverInfo, setHoverInfo] = useState();  
   const deckRef = useRef(null);
-
-  const onClick = useCallback(event => {
-    const pickInfo = deckRef.current.pickObject({
-      x: event.clientX,
-      y: event.clientY,
-      radius: 1
-    });
-    console.log(pickInfo.coordinate, pickInfo.object);
-  }, [])
-
-  const onHover = useCallback(event => {
-    const hoverInfo = deckRef.current.onHover({
-      x: event.clientX,
-      y: event.clientY,
-      radius: 1
-    });
-    console.log(hoverInfo.coordinate, hoverInfo.object);
-  }, [])
-
-
-  // function getTooltip({object}) {
-  //   return object && object.properties;
-  // }
 
   const getTooltip = info => {
     if (!info.object) {
       return null;
     }
-
-
-    return `${info.object.lat} per 100K residents`;
+    return `\
+    ${info.object.desc}
+    ${info.object.id}
+    ${info.object.elevation}`;
   };
+
+  // const onClick = info => {
+  //   if (!info.object) {
+  //     return null;
+  //   }
+  //   console.log(info.devicePixel);
+  //   //console.log(event);
+  //   return (
+  //     <div anchor="top" className="tooltip interactive" style={{left: info.devicePixel[0], top: info.devicePixel[0]}}>
+  //       <div key={info.object.desc}>
+  //         <h5>{info.object.desc}</h5>
+  //         <div>{info.object.id}</div>
+  //         <div>{info.object.elevation}</div>
+  //      </div>
+  //     </div>
+  //   )
+  //   };
   
-//*** 
+
+  const [selected, setSelected] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [showPopup, setShowPopup] = React.useState(true);
+
+  // next example
+  const imageUrl = "127.0.0.1:5000/RIFLE%20GARFIELD%20CO%20AP";
+  const [img, setImg] = useState();
+  const fetchImage = async () => {
+    const res = await fetch(imageUrl);
+    const imageBlob = await res.blob();
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    setImg(imageObjectURL);
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
+ 
+
+
+  const queryClient = new QueryClient();
+  const [imageData, setImageData] = useState();
+  function Example() {
+       const { isLoading, error, data } = useQuery('repoData', () =>
+          fetch('127.0.0.1:5000/RIFLE%20GARFIELD%20CO%20AP')
+          .then(response => response.blob())
+          .then(image => URL.createObjectURL(image))
+          .then(localUrl => setImageData(localUrl))
+          .then(res => console.log(res))
+            // // Create a local URL of that image
+            // const localUrl = URL.createObjectURL(image);
+            // setImageData(localUrl);
+
+        //  .then(res => res.blob())
+        //  .then(resBlob => URL.createObjectURL(resBlob))
+        //  .then(ImageObjectURL => setImg(ImageObjectURL))
+        //  .then(res => console.log(img))
+        //   //URL.createObjectURL(res.blob())
+         //.then(res => console.log(res))
+       );
+
+       if (isLoading) return 'Loading...';
+       if (error) return 'An error has occurred: ' + error.message;
+       return (
+          <div>{data}
+            
+           <img src={data}/>
+          </div>
+       )
+     };
 
   return (
-   
-    /* DECKGL WORKING EXAMPLE */
-    <div onClick={onClick}>
-    <Map
-    {...viewState}
-    onMove={evt => setViewState(evt.viewState)}
-      style={{width: '100vw', height: '100vh'}}
-      mapStyle="mapbox://styles/erickerney/cl0l6ydmk000d14slnsws819a"
-      mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-    >
-      
+    <>
+
+
     <DeckGL 
       ref={deckRef}
       initialViewState={viewState} 
       controller={true}
       layers={[layerStations]}
-      getTooltip={getTooltip}
-      // getTooltip={() => ({
-      //   text: 'Testing tooltip text'
-      // })}
+      ContextProvider={MapContext.Provider}
+      onClick={({ x, y, coordinate, object}) => {
+        // TODO: figure out how to get rid of extra click event
+        console.log(queryClient, object);
+        if (object) {
+          setSelected({ x, y, coordinate, object });
+        } else {
+          // clicked off an object
+          setSelected(null);
+        }
+      }}
+      //getTooltip={getTooltip}
+    >
+      
+      <Map reuseMaps
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        style={{width: '100vw', height: '100vh'}}
+        mapStyle="mapbox://styles/erickerney/cl0l6ydmk000d14slnsws819a"
+        mapboxAccessToken={MAPBOX_ACCESS_TOKEN}   
       >
-        
-      <Map 
-        mapStyle="mapbox://styles/erickerney/cl0l6ydmk000d14slnsws819a" 
-        mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-        
-      />
-     
-      {/* <LineLayer id="line-layer" data={data} /> */}
-    </DeckGL>
-    
-    </Map>
-  </div>
+       {selected && (
+          <Popup
+            longitude={selected.coordinate[0]}
+            latitude={selected.coordinate[1]}
+            closeButton={false}
+            anchor="right"
+            offsetLeft={10}
+            closeOnClick={true}
+            onOpen = {(e, i) => console.log(selected.object)}
+          >
+            <div className="tooltip interactive">
+              {showPopup && (
+                <div>
+                 <iframe 
+                 src={`http://127.0.0.1:5000/${selected.object.desc}`} />
+                </div>
+              )}
+            </div>
+          </Popup>
+        )}
+        </Map>
 
-    /* REACT MAP GL WORKING EXAMPLE */
-    // <Map
-    // {...viewState}
-    // onMove={evt => setViewState(evt.viewState)}
-    //   style={{width: '100vw', height: '100vh'}}
-    //   mapStyle="mapbox://styles/erickerney/cl0l6ydmk000d14slnsws819a"
-    //   mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-    // >
-   
-    // <Source id="my-data" type="geojson" data={geojson}>
-    //   <Layer {...layerStyle} />
-    // </Source>
-    // </Map>
+      
+      
+    </DeckGL>
+    <ControlPanel />
+    </>
   );
 }
 
--- src/control-panel.js
+++ src/control-panel.js
@@ -1,38 +1,26 @@
 import * as React from 'react';
 
-function ControlPanel(props) {
-  const {year} = props;
-
+function ControlPanel() {
   return (
     <div className="control-panel">
-      <h3>Interactive GeoJSON</h3>
+      <h3>Marker, Popup, NavigationControl and FullscreenControl </h3>
       <p>
-        Map showing median household income by state in year <b>{year}</b>. Hover over a state to
-        see details.
+        Map showing top 20 most populated cities of the United States. Click on a marker to learn
+        more.
       </p>
       <p>
-        Data source: <a href="www.census.gov">US Census Bureau</a>
+        Data source:{' '}
+        <a href="https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population">
+          Wikipedia
+        </a>
       </p>
       <div className="source-link">
         <a
-          href="https://github.com/visgl/react-map-gl/tree/7.0-release/examples/geojson"
+          href="https://github.com/visgl/react-map-gl/tree/7.0-release/examples/controls"
           target="_new"
         >
           View Code ↗
         </a>
-      </div>
-      <hr />
-
-      <div key={'year'} className="input">
-        <label>Year</label>
-        <input
-          type="range"
-          value={year}
-          min={1995}
-          max={2015}
-          step={1}
-          onChange={evt => props.onChange(evt.target.value)}
-        />
       </div>
     </div>
   );
