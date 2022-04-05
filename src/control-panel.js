1825c4a33187c1f7e90e7f9e631deb8897897b00
eKerney <Eric.Kerney@airspacelink.com>
2022-04-04 21:27:47 -0400

    still working

--- src/control-panel.js
+++ src/control-panel.js
@@ -4,8 +4,8 @@ import Box from '@mui/material/Box';
 import {useState, useEffect, useMemo, useCallback, useRef} from 'react';
 import * as ReactDOM from 'react-dom';
 
-// prodURL = 'https://noaaflaskapi.herokuapp.com/station/'
-// localURL = 'http://127.0.0.1:5000/'
+const prodURL = 'https://noaaflaskapi.herokuapp.com/station/', localURL = 'http://127.0.0.1:5000/year/';
+const localURLmonth = 'http://127.0.0.1:5000/month';
 
 function ControlPanel(props) {
   const marks = [
@@ -22,25 +22,24 @@ function ControlPanel(props) {
     {value: 100, label: 'NOV'},
     {value: 110, label: 'DEC'},
   ];
-  
   const [month, setMonth] = useState(null);
 
-  const onChange = (e, v) => console.log(e, v);
-  const OnChangeCommitted = (e, v) => {
-    //console.log(props);
-    //console.log(e, v);
-    };
+  // const onChange = (e, v) => console.log(e, v);
+  // const OnChangeCommitted = (e, v) => {
+  //   //console.log(props);
+  //   //console.log(e, v);
+  //   };
+
   const valueLabelFormat = (val) => `${(marks.find(m => m.value === val)).label}`;
   useEffect(() => {
-    //console.log(props, month.mon);
-    
     const panel = !month ? '' : document.getElementsByClassName('control-panel');
-    console.log(panel);
-
-  
+    //  console.log(panel);
+    month && (console.log(month));
   });
 
-  return !props.station ? (
+  return !props.station ? 
+  // START PANEL NO SLIDER
+  (
     <div className="control-panel">
       <h2>Operational Weather Suitability</h2>
       <p>NOAA GHCN Stations Climate Normals</p>
@@ -49,65 +48,59 @@ function ControlPanel(props) {
       <br />
     </div>
   ) 
-    : ( 
-    
-      <div className="control-panel" style={{height: '800px'}}>
-        <h2>Operational Weather Suitability</h2>
-        <p>NOAA GHCN Stations Climate Normals</p>
-        <p>Click on a Station for Climate Chart - Data: <a href="https://www.ncdc.noaa.gov/cdo-web/" target="_blank" rel="noopener noreferrer">NOAA Climate Data</a></p>
-        
-        <hr style={{width: '800px', marginLeft: '-100px', marginBottom: '10px', marginTop: '26px'}}/>
-        <br />
-        <div style={{position: 'absolute'}}>
-          <iframe src={`https://noaaflaskapi.herokuapp.com/station/${props.station}`}></iframe>
-        </div>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-         <hr style={{width: '800px', marginLeft: '-100px'}}/>
-         <p></p>
-         <Slider
-         aria-label="Temperature"
-         defaultValue={30}
-         //getAriaValueText={valuetext}
-         valueLabelDisplay="auto"
-         step={10}
-         marks={marks}
-         min={0}
-         max={110}
-         valueLabelFormat={valueLabelFormat}
-         //onChange={onChange}
-         onChangeCommitted={(e, v) => {
-           //console.log(props, e, v, `${(marks.find(m => m.value === v)).label}`);
-           const mon = `${(marks.find(m => m.value === v)).label}`;
-           setMonth({props, mon});
-           
-           } 
-         }
-       />
+  // END PANEL NO SLIDER
+    : 
+    props.station && month ? (
+    <div className="control-panel" style={{height: '1000px'}}>
+      <h2>Operational Weather Suitability{month.mon}</h2>
+      <p>NOAA GHCN Stations Climate Normals</p>
+      <p>Click on a Station for Climate Chart - Data: <a href="https://www.ncdc.noaa.gov/cdo-web/" target="_blank" rel="noopener noreferrer">NOAA Climate Data</a></p>
+      <hr style={{width: '800px', marginLeft: '-100px', marginBottom: '10px', marginTop: '26px'}}/>
+      <br />
+      <div style={{position: 'absolute'}}>
+        <iframe src={`${localURL}${props.station}`} />
       </div>
-    
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+       <hr style={{width: '800px', marginLeft: '-100px'}}/>
+       <p></p>
+        <Slider name='monthSlider' aria-label="Temperature" defaultValue={0} valueLabelDisplay="auto" step={10} marks={marks}
+          min={0} max={110} valueLabelFormat={valueLabelFormat}
+          onChangeCommitted={(e, v) => setMonth({mon: `${(marks.find(m => m.value === v)).label}`, sta: props.station})}
+        />
+        <div style={{position: 'absolute'}}>
+        <iframe src={`${localURLmonth}?station=${props.station}&month=${month.mon}`} />
+      </div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <hr style={{width: '800px', marginLeft: '-100px'}}/>
+       <p></p>
+        <Slider name='daySlider' aria-label="Temperature" defaultValue={0} valueLabelDisplay="auto" step={10} marks={marks}
+          min={0} max={110} valueLabelFormat={valueLabelFormat}
+          onChangeCommitted={(e, v) => setMonth({mon: `${(marks.find(m => m.value === v)).label}`, sta: props.station})}
+        />
+    </div>
+    ) :
+    // START PANEL WITH MONTH SLIDER
+    (     
+      <div className="control-panel" style={{height: '800px'}}>
+      <h2>Operational Weather Suitability</h2>
+      <p>NOAA GHCN Stations Climate Normals</p>
+      <p>Click on a Station for Climate Chart - Data: <a href="https://www.ncdc.noaa.gov/cdo-web/" target="_blank" rel="noopener noreferrer">NOAA Climate Data</a></p>
+      <hr style={{width: '800px', marginLeft: '-100px', marginBottom: '10px', marginTop: '26px'}}/>
+      <br />
+      <div style={{position: 'absolute'}}>
+        <iframe src={`${localURL}${props.station}`} />
+      </div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+       <hr style={{width: '800px', marginLeft: '-100px'}}/>
+       <p></p>
+        <Slider name='monthSlider' aria-label="Temperature" defaultValue={0} valueLabelDisplay="auto" step={10} marks={marks}
+          min={0} max={110} valueLabelFormat={valueLabelFormat}
+          onChangeCommitted={(e, v) => setMonth({mon: `${(marks.find(m => m.value === v)).label}`, sta: props.station})}
+        />
+    </div>
     ); 
-    // extended panel for additional chart display
-    // ( 
-    
-    //   <div className="control-panel">
-    //     <h2>Operational Weather Suitability</h2>
-    //     <p>NOAA GHCN Stations Climate Normals</p>
-    //     <p>Click on a Station for Climate Chart - Data: <a href="https://www.ncdc.noaa.gov/cdo-web/" target="_blank" rel="noopener noreferrer">NOAA Climate Data</a></p>
-    //     <hr style={{width: '800px', marginLeft: '-100px'}}/>
-    //     <br />
-    //     <div style={{position: 'absolute'}}>
-    //       <iframe src={`https://noaaflaskapi.herokuapp.com/station/${props.station}`}></iframe>
-    //     </div>
-    //     <div style={{position: 'absolute'}}>
-    //     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      
-    //     <hr style={{width: '800px', marginLeft: '-100px'}}/>
-    //     <p></p>
-    //     </div>
-    //   </div>
-    
-    // );
+    // END PANEL WITH MONTH SLIDER
     
 }
 
-export default React(ControlPanel);
+export default React.memo(ControlPanel);
